a86695a91ad2c3ae7fde5a88f86a74bd241d0693
JerMa25 <tchamijerry@gmail.com>
2025-11-20 23:22:27 +0100

    Recherche opérationnelle dans la page AdminMedicalStaffList
    Ajout du filtre par rapport au role

--- ihm_fultang-frontend-v1/src/Pages/AdminViews/AddMedicalStaff.jsx
+++ ihm_fultang-frontend-v1/src/Pages/AdminViews/AddMedicalStaff.jsx
@@ -123,9 +123,6 @@ export function AddMedicalStaff() {
                             {/*Make sure the information entered is accurate as it will be used to create the staff member's official profile.*/}
 
                     <img src={joinOurStaffImage} alt={"image"} className={"w-[600px] h-[400px] rounded-2xl"}/>
-                    <p className='"text-justify text-red-500 font-normal text-md mb-5"'>
-                        Make sure the information entered is accurate as it will be used to create the staff member's official profile.
-                    </p>
                 </div>
                 <div className="w-1/2 p-8 flex items-center justify-center">
                     <form onSubmit={handleSubmit} className="space-y-6">
