d0aed1d54d9f03b5d6886140e4ce8c4263f82622
miguelttech <njikitchoubiamiguelalan@gmail.com>
2026-01-17 12:51:19 +0100

    Mise à jour du frontend et corrections de la branche

--- ihm_fultang-frontend-v1/src/Pages/AdminViews/AdminNavBar.jsx
+++ ihm_fultang-frontend-v1/src/Pages/AdminViews/AdminNavBar.jsx
@@ -1,12 +1,11 @@
-import {FaCog, FaEnvelope, FaSignOutAlt} from "react-icons/fa";
-import {Tooltip} from "antd";
-import {useAuthentication} from "../../Utils/Provider.jsx";
+import { FaCog, FaEnvelope, FaSignOutAlt } from "react-icons/fa";
+import { Tooltip } from "antd";
+import { useAuthentication } from "../../Utils/Provider.jsx";
 import userIcon from "../../assets/userIcon.png";
 
-export function AdminNavBar()
-{
+export function AdminNavBar() {
 
-    const {logout , userData} = useAuthentication();
+    const { logout, userData } = useAuthentication();
 
     const applyNavLinkBtnStyle = () => {
         return " w-12 h-10 mt-1 border-2 bg-gray-100 flex justify-center items-center rounded-xl shadow-xl hover:bg-secondary text-secondary text-xl hover:text-white transition-all duration-300";
@@ -20,8 +19,8 @@ export function AdminNavBar()
                     <h1 className="ml-3 text-4xl text-secondary mt-3.5 font-bold">
                         Administrator
                     </h1>
-                     <div className="flex gap-3 mt-3.5 mb-4 mr-5">
-                         {/* <Tooltip placement={"top"} title={"settings"}>
+                    <div className="flex gap-3 mt-3.5 mb-4 mr-5">
+                        {/* <Tooltip placement={"top"} title={"settings"}>
                             <button className={applyNavLinkBtnStyle()}>
                                 <FaCog/>
                             </button>
@@ -35,16 +34,15 @@ export function AdminNavBar()
                         </Tooltip> */}
                         <Tooltip placement={"top"} title={"LogOut"}>
                             <button
-                                onClick={() => {logout()}}
+                                onClick={() => { logout() }}
                                 className={" w-12 h-10 mt-1 border-2 bg-red-400 flex justify-center items-center rounded-xl shadow-xl hover:bg-white text-white text-xl hover:text-red-500 transition-all duration-300"}>
-                                <FaSignOutAlt/>
+                                <FaSignOutAlt />
                             </button>
                         </Tooltip>
                         <Tooltip placement={"top"} title={"Profile"}>
                             <div className="ml-3 flex">
-                                <p className="font-bold text-secondary text-xl mt-2">{"Hello " +userData?.username + "!"}</p>
-                                <img src={userIcon} alt={"user-icon"} className="w-12 h-12 ml-2 mr-3"/>
-
+                                <p className="font-bold text-secondary text-xl mt-2">{"Hello " + userData?.username + "!"}</p>
+                                <img src={userIcon} alt={"user-icon"} className="w-12 h-12 ml-2 mr-3" />
                             </div>
                         </Tooltip>
                     </div>
